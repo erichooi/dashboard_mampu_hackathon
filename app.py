4eeb7d72bd3ac8c8facfbb83f1da65938d65c961
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2019-09-11 22:51:28 +0800

    update link

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def main():
 	return render_template("index.html")
 
 if __name__ == "__main__":
-	# app.run(debug=True)
+	# app.run(debug=True, port=8000)
 	app.run(port=8000)
